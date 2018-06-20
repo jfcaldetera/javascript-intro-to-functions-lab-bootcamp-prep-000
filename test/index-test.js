d03cc43e65f8fe260dfc92f61b27fc57c18280af
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-20 16:41:10 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -68,3 +68,19 @@ function logWhisper(string)
 {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string)
+{
+  if (string == string.toUpperCase())
+  {
+    return ("I can\'t hear you!")
+  }
+  else if (string == string.toLowerCase())
+  {
+    return ("YES INDEED!")
+  }
+  else if (string == "I love you, Grandma.")
+  {
+    return ("I love you, too.")
+  }
+}
