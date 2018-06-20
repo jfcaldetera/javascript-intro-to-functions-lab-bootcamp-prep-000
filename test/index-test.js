f3889178912e24ae877ba3be4cf4e714473eeea2
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-20 16:42:00 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -71,11 +71,11 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  if (string == string.toUpperCase())
+  if (string == string.toLowerCase())
   {
     return ("I can\'t hear you!")
   }
-  else if (string == string.toLowerCase())
+  else if (string == string.toUpperCase())
   {
     return ("YES INDEED!")
   }
